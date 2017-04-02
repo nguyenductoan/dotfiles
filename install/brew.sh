760b34c42a0e4389160ac5b14a2aedb045ce903b
nguyenductoan <ductoan593@gmail.com>
2017-04-02 16:52:58 +0700

    Brew: init script install brew package

---
+++ install/brew.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+if test ! $(which brew); then
+    echo "Installing homebrew"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+echo -e "\n\nInstalling homebrew packages..."
+echo "=============================="
+
+formulas=(
+    # flags should pass through the the `brew list check`
+    ack
+    diff-so-fancy
+    fzf
+    git
+    highlight
+    markdown
+    neovim/neovim/neovim
+    the_silver_searcher
+    tmux
+    wget
+)
+
+for formula in "${formulas[@]}"; do
+    if brew list "$formula" > /dev/null 2>&1; then
+        echo "$formula already installed... skipping."
+    else
+        brew install $formula
+    fi
+    echo -e "\n\n"
+done
+
