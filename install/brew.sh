039299ab4524a46cc6c52b018f686cfd6af46d17
nguyenductoan <ductoan593@gmail.com>
2017-10-01 17:02:51 +0700

    INSTALL: add rails_env install, change brew.sh

--- install/brew.sh
+++ install/brew.sh
@@ -42,20 +42,38 @@ for formula in "${formulas[@]}"; do
     echo -e "\n\n"
 done
 
+echo "Start mysql service"
+brew services start mysql
+
+echo "installing neovim"
 pip3 install neovim
 
+echo "installing Oh My Zsh via wget"
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+
+echo -e "\n\nCopy config file..."
+echo "=============================="
+
 echo "copy gitcofig to root directory"
-cp ~/dotfiles/git/.gitconfig ~/
+cp ../git/.gitconfig ~/
+
+echo "copy inputrc config file"
+cp ..config/.inputrc ~/
 
 echo "copy neovim config to root directory"
-cp ~/dotfiles/config/nvim/init.vim ~/
+if [ ! -d "~/.config/nvim" ]; then
+  mkdir -p ~/.config/nvim
+fi
+cp ../config/nvim/init.vim ~/.config/nvim/
 
 echo "copy zsh config to root directory"
-cp ~/dotfiles/zsh/.zshrc ~/
+cp ../zsh/.zshrc ~/
 
 echo "copy tmux config to root directory"
-cp ~/dotfiles/tmux/.tmux.conf ~/
+cp ../tmux/.tmux.conf ~/
 
-echo "install Oh My Zsh via wget"
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+echo "copy tmux config environment to root directory"
+cp ../tmux/general-dev ~/
+cp ../tmux/lixibox-dev ~/
 
---
+++ install/rails_env/lixibox.sh
@@ -0,0 +1,20 @@
+echo "installing RVM"
+\curl -sSL https://get.rvm.io | bash
+source /Users/nguyenductoan/.rvm/scripts/rvm
+
+echo "installing ruby"
+rvm install 2.3.3
+
+echo "creating gem set"
+rvm gemset create lixibox
+rvm list gemsets
+rvm gemset use lixibox
+
+echo "Installing Rails"
+gem install rails -v 4.2.1
+
+echo "install qt"
+brew install qt@5.5
+echo "force Homebrew to symlink qt binaries"
+brew link --force qt@5.5
+
